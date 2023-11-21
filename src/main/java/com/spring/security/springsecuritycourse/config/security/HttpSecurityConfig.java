8b1c21ae041bae782cc821ed9b7ad3b34827667f
Joxxx69 <santiago.gualotuna@hotmail.com>
2023-11-21 00:06:12 -0500

    Authorization integrada

--- src/main/java/com/spring/security/springsecuritycourse/config/security/HttpSecurityConfig.java
+++ src/main/java/com/spring/security/springsecuritycourse/config/security/HttpSecurityConfig.java
@@ -10,6 +10,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.spring.security.springsecuritycourse.config.security.filter.JwtAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity//configura por default los elementos de se seguridad(authenticationConfiguration etc)
@@ -17,6 +21,9 @@ public class HttpSecurityConfig {
 
     @Autowired
     private AuthenticationProvider daoAuthProvider;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
     
     //HttpSecurity -> permite personalizar como se gestionan y protegen las peticiones http
     @Bean
@@ -26,6 +33,7 @@ public class HttpSecurityConfig {
             .csrf(csrfConfig -> csrfConfig.disable()) // desactiva el uso de tokens ya que usaremos Jwt
             .sessionManagement(sessMagConfig->sessMagConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // sesion sin estado
             .authenticationProvider(daoAuthProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .authorizeHttpRequests(authReqConfig->{  //configuracion de rutas publicas
                     authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
                     authReqConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
---
+++ src/main/java/com/spring/security/springsecuritycourse/config/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.spring.security.springsecuritycourse.config.security.filter;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.spring.security.springsecuritycourse.exception.ObjectNotFoundException;
+import com.spring.security.springsecuritycourse.persistence.entity.User;
+import com.spring.security.springsecuritycourse.service.UserService;
+import com.spring.security.springsecuritycourse.service.auth.JwtService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        System.out.println("ENTRO EN EL FILTRO JWT AUTHENTICATION FILTER");
+        //1- Obtner encabezado http llamado Authorization
+        String authorizationHeader = request.getHeader("Authorization");
+        if (!StringUtils.hasText(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")) {
+            // para que siga con los demas filtros, encaso de que el header no tenga el token
+            filterChain.doFilter(request, response);
+            return; // retorna el control a quien mando a llamar el metodo actual
+        }
+            
+
+        //2- Obtener token JWT desde el header(encabezado)
+
+        String jwt = authorizationHeader.split(" ")[1];
+        //String jwt2 = authorizationHeader.substring(7);
+
+        //3- Obtener el subject/username desde el token
+        // esta accion a su vez valida el formato del token, firma y fecha de expiracion 
+        String username = jwtService.extractUsername(jwt);
+
+
+        //4- Setear objeto authentication(usuario logeado) dentro de security contex holder
+        User user = userService.findOneByUsername(username)
+                .orElseThrow(() -> new ObjectNotFoundException("User not found Username: " + username));
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username,null, user.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        //5- Ejecutar el registro de filtros
+
+        filterChain.doFilter(request, response);
+    }
+    
+}
